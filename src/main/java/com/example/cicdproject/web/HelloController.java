2b336492e6cc380fd32b5285a4d82f7b3ac1d1ae
KJS <otw1917@naver.com>
2023-05-20 09:42:20 +0900

    Jpa설정

---
+++ src/main/java/com/example/cicdproject/web/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.cicdproject.web;
+
+import com.example.cicdproject.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+
+        return new HelloResponseDto(name, amount);
+    }
+}
---
+++ src/main/java/com/example/cicdproject/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,25 @@
+package com.example.cicdproject.web.dto;
+
+import com.example.cicdproject.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+        return new Posts(title, content, author);
+    }
+}
